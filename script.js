55c5db730465cb4d5338ad0a30099a7ec484b1dc
Dawn Kim <70921363+dkim3191@users.noreply.github.com>
2025-03-09 15:37:01 -0400

    todo: make form for button

--- script.js
+++ script.js
@@ -1,42 +1,54 @@
 let books = [];
 
-function Book(title, author) {
+function Book(title, author, pages, have_read) {
     this.title = title;
     this.author = author;
+    this.pages = pages;
+    this.have_read = have_read;
     this.id = crypto.randomUUID();
 }
 
-function createBook(title, author) {
-    const book = new Book(title, author);
+function createBook(title, author, pages, have_read) {
+    const book = new Book(title, author, pages, have_read);
     books.push(book);
-    displayBook(book);
 }
 
-function displayBook(book) {
-    const library = document.querySelector(".library");
+createBook("One Happy Day", "Rosemarry Johnson", 352, "yes");
+createBook("The Weeping Willow", "Harry Potter", 142, "no");
+createBook("The Last One Standing", "Bill Ross", 535, "yes");
+createBook("How Many Men", "Patricia Lakes", 523, "no");
 
-    const div = document.createElement("div");
-    div.className = "book";
-    const title = document.createElement("div");
-    title.className = "title";
-    title.textContent = book.title;
-    const author = document.createElement("div");
-    author.className = "author";
-    author.textContent = book.author;
-    const id = document.createElement("div");
-    id.textContent = book.id;
+const library = document.querySelector(".library");
 
-    library.appendChild(div);
-    div.appendChild(title);
-    div.appendChild(author);
-    div.appendChild(id);
+function displayBooks(books) {
+    for (let book of books) {
+        const div = document.createElement("div");
+        const title = document.createElement("h2");
+        const author = document.createElement("h4");
+        const pages = document.createElement("p");
+        const have_read = document.createElement("p");
+        const id = document.createElement("p");
+    
+        div.className = "book";
+        title.className = "title";
+        author.className = "author";
+        pages.className = "pages";
+        have_read.className = "have_read";
+        id.className = "id";
+        
+        title.textContent = book.title;
+        author.textContent = book.author;
+        pages.textContent = book.pages + " pages";
+        have_read.textContent = "Have read: " + book.have_read;
+        id.textContent = "ID: " + book.id;
+    
+        library.appendChild(div);
+        div.appendChild(title);
+        div.appendChild(author);
+        div.appendChild(pages);
+        div.appendChild(have_read);
+        div.appendChild(id);
+    }
 }
 
-createBook("One Happy Day", "Rosemarry Johnson");
-createBook("The Weeping Willow", "Harry Potter");
-createBook("The Last One Standing", "Bill Ross");
-createBook("How Many Men", "Patricia Lakes");
-
-for (let book of books) {
-    console.log(book);
-}
+displayBooks(books);
