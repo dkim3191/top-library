8b9a7cd4046f2492c8195eb517b8530c81b72738
dkim3191 <dkim3191@gmail.com>
2025-03-10 14:58:28 -0400

    Library complete

--- script.js
+++ script.js
@@ -13,50 +13,77 @@ function createBook(title, author, pages, have_read) {
     books.push(book);
 }
 
-createBook("One Happy Day", "Rosemarry Johnson", 352, "yes");
-createBook("The Weeping Willow", "Harry Potter", 142, "no");
-createBook("The Last One Standing", "Bill Ross", 535, "yes");
-createBook("How Many Men", "Patricia Lakes", 523, "no");
+createBook("One Happy Day", "Rosemarry Johnson", 352, "Yes");
+createBook("The Weeping Willow", "Harry Potter", 142, "No");
+createBook("Cinderella", "James Bond", 150, "Yes");
+createBook("How Come?", "George Clooney", 992, "No");
 
 const library = document.querySelector(".library");
 
 function displayBooks(books) {
+    library.innerHTML = "";
     for (let book of books) {
+        const bookWrapper = document.createElement("div");
         const div = document.createElement("div");
         const title = document.createElement("h2");
         const author = document.createElement("h4");
         const pages = document.createElement("p");
         const have_read = document.createElement("p");
-        const id = document.createElement("p");
-    
+        const removeBtn = document.createElement("button");
+
+        bookWrapper.className = "book-wrapper";
         div.className = "book";
         title.className = "title";
         author.className = "author";
         pages.className = "pages";
         have_read.className = "have_read";
-        id.className = "id";
-        
+        removeBtn.className = "remove-button";
+        removeBtn.setAttribute("data-book-id", book.id);
+
         title.textContent = book.title;
         author.textContent = book.author;
         pages.textContent = book.pages + " pages";
         have_read.textContent = "Have read: " + book.have_read;
-        id.textContent = "ID: " + book.id;
-    
-        library.appendChild(div);
+        removeBtn.textContent = "Remove";
+
+        removeBtn.addEventListener("click", (e) => {
+            const bookID = e.target.dataset.bookId;
+            for (const b in books) {
+                if (bookID == books[b].id) {
+                    bookWrapper.remove();
+                    books.splice(b, 1);
+                }
+            }
+        });
+
+        library.appendChild(bookWrapper);
+        bookWrapper.appendChild(div);
         div.appendChild(title);
         div.appendChild(author);
         div.appendChild(pages);
         div.appendChild(have_read);
-        div.appendChild(id);
+        bookWrapper.appendChild(removeBtn);
     }
 }
 
 displayBooks(books);
 
-
-
 // Dialog
 const showBtn = document.querySelector(".new-book");
 const dialog = document.querySelector("dialog");
 const addBtn = document.querySelector(".add-btn");
-const cancelBtn = document.querySelector(".cancel-btn");
+
+showBtn.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+addBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const inp_title = document.querySelector("#title").value;
+    const inp_author = document.querySelector("#author").value;
+    const inp_pages = document.querySelector("#pages").value;
+    const inp_have_read = document.querySelector("#have-read").checked ? "Yes" : "No";
+    createBook(inp_title, inp_author, inp_pages, inp_have_read);
+    displayBooks(books);
+    dialog.close();
+});
